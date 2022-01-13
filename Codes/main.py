7cf027765ea2bbc75400c9f113dca35d1c40d025
Kiiaan <46507199+Kiiaan@users.noreply.github.com>
2022-01-13 09:53:28 -0800

    Update main.py

--- Codes/main.py
+++ Codes/main.py
@@ -7,49 +7,49 @@ import subprocess as sp
 param_file = "params.yaml"
 params = yaml.safe_load(open(param_file, "r"))    
 
-# """ Running image registration"""
-# align_shell = [sys.executable, "AlignerDriver.py", param_file]
-# print(align_shell)
-# commandOut = sp.run(align_shell, stderr = sp.PIPE, text = True)
+""" Running image registration"""
+align_shell = [sys.executable, "AlignerDriver.py", param_file]
+print(align_shell)
+commandOut = sp.run(align_shell, stderr = sp.PIPE, text = True)
 
-# if commandOut.stderr != '':
-#     print(commandOut.stderr)
-#     sys.exit(1)
+if commandOut.stderr != '':
+    print(commandOut.stderr)
+    sys.exit(1)
 
-# """ Running background subtraction (optional)"""
-# if params['background_subtraction']:
-#     subt_shell = [sys.executable, "backgroundSubtraction.py", param_file]
-#     print(subt_shell)
-#     commandOut = sp.run(subt_shell, stderr = sp.PIPE, text = True)
+""" Running background subtraction (optional)"""
+if params['background_subtraction']:
+    subt_shell = [sys.executable, "backgroundSubtraction.py", param_file]
+    print(subt_shell)
+    commandOut = sp.run(subt_shell, stderr = sp.PIPE, text = True)
  
-# print(commandOut.stderr)
+print(commandOut.stderr)
 
-# if commandOut.returncode:
-#     print("Error in background subtraction:")
-#     sys.exit(1)
+if commandOut.returncode:
+    print("Error in background subtraction:")
+    sys.exit(1)
     
-# """ Running stitching"""
-# stitch_shell = [sys.executable, "StitchDriver.py", param_file]
-# print(stitch_shell)
-# commandOut = sp.run(stitch_shell, stderr = sp.PIPE, text = True)
+""" Running stitching"""
+stitch_shell = [sys.executable, "StitchDriver.py", param_file]
+print(stitch_shell)
+commandOut = sp.run(stitch_shell, stderr = sp.PIPE, text = True)
 
-# print(commandOut.stderr)
-# if commandOut.returncode:
-#     print("Error in stitching")
-#     print(commandOut.stderr)
-#     sys.exit(1)
+print(commandOut.stderr)
+if commandOut.returncode:
+    print("Error in stitching")
+    print(commandOut.stderr)
+    sys.exit(1)
 
-# """ to StarFish format"""
-# tosff_shell = [sys.executable, "toStarfishFormat.py", param_file]
-# print(tosff_shell)
-# commandOut = sp.run(tosff_shell, stderr = sp.PIPE, text = True)
+""" to StarFish format"""
+tosff_shell = [sys.executable, "toStarfishFormat.py", param_file]
+print(tosff_shell)
+commandOut = sp.run(tosff_shell, stderr = sp.PIPE, text = True)
 
-# print(commandOut.stderr)
+print(commandOut.stderr)
 
-# if commandOut.returncode:
-#     print("Error in toStarfishFormat:")
-#     print(commandOut.stderr)
-#     sys.exit(1)
+if commandOut.returncode:
+    print("Error in toStarfishFormat:")
+    print(commandOut.stderr)
+    sys.exit(1)
 
 # """ Run Starfish decoding"""
 dc_shell = [sys.executable, "starfishDARTFISHpipeline.py", param_file]
