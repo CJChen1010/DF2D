dd8e0bdee70f09f523ea686d4c2ed597f0985ba9
Kiiaan <kian.klhr@gmail.com>
2023-02-06 16:41:01 -0800

    Adaptive regularization

--- Codes/main.py
+++ Codes/main.py
@@ -11,50 +11,60 @@ args = parser.parse_args()
 params = yaml.safe_load(open(args.param_file, "r"))
 param_file = args.param_file
 
-# """ Running maximum intensity projection"""
-# mip_shell = [sys.executable, "maxProjection.py", param_file]
-# print(mip_shell)
-# commandOut = sp.run(mip_shell, stderr = sp.PIPE, text = True)
+""" Running maximum intensity projection"""
+mip_shell = [sys.executable, "maxProjection.py", param_file]
+print(mip_shell)
+commandOut = sp.run(mip_shell, stderr = sp.PIPE, text = True)
 
-# print(commandOut.stderr)
+print(commandOut.stderr)
 
-# if commandOut.returncode:
-#     print("Error in MIPing.")
-#     sys.exit(1)
+if commandOut.returncode:
+    print("Error in MIPing.")
+    sys.exit(1)
 
 
-# """ Running image registration"""
-# align_shell = [sys.executable, "AlignerDriver.py", param_file]
-# print(align_shell)
-# commandOut = sp.run(align_shell, stderr = sp.PIPE, text = True)
+""" Running image registration"""
+align_shell = [sys.executable, "AlignerDriver.py", param_file]
+print(align_shell)
+commandOut = sp.run(align_shell, stderr = sp.PIPE, text = True)
 
-# print(commandOut.stderr)
+print(commandOut.stderr)
 
-# if commandOut.returncode:
-#     print("Error in image registration.")
-#     sys.exit(1)
+if commandOut.returncode:
+    print("Error in image registration.")
+    sys.exit(1)
 
-# """ Running background subtraction (optional)"""
-# if params['background_subtraction']:
-#     subt_shell = [sys.executable, "backgroundSubtraction.py", param_file]
-#     print(subt_shell)
-#     commandOut = sp.run(subt_shell, stderr = sp.PIPE, text = True)
+""" Running background subtraction (optional)"""
+if params['background_subtraction']:
+    subt_shell = [sys.executable, "backgroundSubtraction.py", param_file]
+    print(subt_shell)
+    commandOut = sp.run(subt_shell, stderr = sp.PIPE, text = True)
  
-#     print(commandOut.stderr)
+    print(commandOut.stderr)
 
-#     if commandOut.returncode:
-#         print("Error in background subtraction:")
-#         sys.exit(1)
+    if commandOut.returncode:
+        print("Error in background subtraction:")
+        sys.exit(1)
     
-# """ Running stitching"""
-# stitch_shell = [sys.executable, "StitchDriver.py", param_file]
-# print(stitch_shell)
-# commandOut = sp.run(stitch_shell, stderr = sp.PIPE, text = True)
+""" Running stitching"""
+stitch_shell = [sys.executable, "StitchDriver.py", param_file]
+print(stitch_shell)
+commandOut = sp.run(stitch_shell, stderr = sp.PIPE, text = True)
 
-# print(commandOut.stderr)
-# if commandOut.returncode:
-#     print("Error in stitching.")
-#     sys.exit(1)
+print(commandOut.stderr)
+if commandOut.returncode:
+    print("Error in stitching.")
+    sys.exit(1)
+
+""" Run the pre-decoding code"""
+pdc_shell = [sys.executable, "findDecodingParams.py", param_file]
+print(pdc_shell)
+commandOut = sp.run(pdc_shell, stderr = sp.PIPE, text = True)
+
+print(commandOut.stderr)
+if commandOut.returncode:
+    print("Error in findDecodingParams.py.")
+    sys.exit(1)
 
 """ Run decoding"""
 dc_shell = [sys.executable, "decoding_driver.py", param_file]
